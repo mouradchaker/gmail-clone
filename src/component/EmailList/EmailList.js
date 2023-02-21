617801a5b8f5f1a43568541aaf21e3352b3b66f9
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-02-22 00:17:47 +0100

    emailList-section

--- src/component/EmailList/EmailList.js
+++ src/component/EmailList/EmailList.js
@@ -8,6 +8,10 @@ import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import KeyboardHideIcon from "@mui/icons-material/KeyboardHide";
 import SettingsIcon from "@mui/icons-material/Settings";
+import InboxIcon from "@mui/icons-material/Inbox";
+import PeopleIcon from "@mui/icons-material/People";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
+import Section from "../Section/Section";
 
 function EmailList() {
   return (
@@ -41,6 +45,12 @@ function EmailList() {
             <SettingsIcon />
           </IconButton>
         </div>
+      </div>
+
+      <div className="emailList__sections">
+        <Section Icon={InboxIcon} title="Primary" color="red" selected />
+        <Section Icon={PeopleIcon} title="Social" color="#1A73E8" />
+        <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
       </div>
     </div>
   );
