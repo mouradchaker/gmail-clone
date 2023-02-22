566827dbe0d595c83255ff35a214dba5466ca975
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-02-22 23:15:45 +0100

    emailRow

--- src/component/EmailList/EmailList.js
+++ src/component/EmailList/EmailList.js
@@ -12,6 +12,7 @@ import InboxIcon from "@mui/icons-material/Inbox";
 import PeopleIcon from "@mui/icons-material/People";
 import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 import Section from "../Section/Section";
+import EmailRow from "../EmailRow/EmailRow";
 
 function EmailList() {
   return (
@@ -51,6 +52,15 @@ function EmailList() {
         <Section Icon={InboxIcon} title="Primary" color="red" selected />
         <Section Icon={PeopleIcon} title="Social" color="#1A73E8" />
         <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
+      </div>
+
+      <div className="emailList__list">
+        <EmailRow
+          title="Twitch"
+          subject="Hey fellow streamer!!"
+          description="This is a test "
+          time="10pm"
+        />
       </div>
     </div>
   );
