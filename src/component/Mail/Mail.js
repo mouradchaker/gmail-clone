f9eb1e3371bb1b7a1bd1c0d950566fb5bbc3ef92
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-02-23 23:49:51 +0100

    mail

--- src/component/Mail/Mail.js
+++ src/component/Mail/Mail.js
@@ -59,7 +59,7 @@ function Mail() {
           </IconButton>
         </div>
 
-        <div className="mail__toolsLeft">
+        <div className="mail__toolsRight">
           <IconButton>
             <UnfoldMoreIcon />
           </IconButton>
@@ -71,6 +71,19 @@ function Mail() {
           <IconButton>
             <ExitToAppIcon />
           </IconButton>
+        </div>
+      </div>
+
+      <div className="mail__body">
+        <div className="mail__bodyHeader">
+          <h2>Subject</h2>
+          <LabelImportantIcon className="mail__important" />
+          <p>Title</p>
+          <p className="mail__time">10pm</p>
+        </div>
+
+        <div className="mail__message">
+          <p>This is a message</p>
         </div>
       </div>
     </div>
