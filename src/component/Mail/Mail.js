4d42db79a6fe571959c760c053b0846fbd3dc8f9
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-02-26 23:49:18 +0100

    mailFunc

--- src/component/Mail/Mail.js
+++ src/component/Mail/Mail.js
@@ -14,9 +14,12 @@ import { useHistory } from "react-router-dom";
 import UnfoldMoreIcon from "@mui/icons-material/UnfoldMore";
 import PrintIcon from "@mui/icons-material/Print";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import { useSelector } from "react-redux";
+import { selectOpenMail } from "../../features/mailSlice";
 
 function Mail() {
   const history = useHistory();
+  const selectedMail = useSelector(selectOpenMail);
 
   return (
     <div className="mail">
@@ -76,14 +79,14 @@ function Mail() {
 
       <div className="mail__body">
         <div className="mail__bodyHeader">
-          <h2>Subject</h2>
+          <h2>{selectedMail?.subject}</h2>
           <LabelImportantIcon className="mail__important" />
-          <p>Title</p>
-          <p className="mail__time">10pm</p>
+          <p>{selectedMail?.title}</p>
+          <p className="mail__time">{selectedMail?.time}</p>
         </div>
 
         <div className="mail__message">
-          <p>This is a message</p>
+          <p>{selectedMail?.description}</p>
         </div>
       </div>
     </div>
